bc1a16289b4d3edd13789327c9b63a846d294769
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2020-05-04 16:21:01 +0200

    PUBDEV-7237 fixes (tests, conflicts, naming, checkpointing)

--- h2o-r/tests/testdir_algos/stackedensemble/runit_stackedensemble_metalearner.R
+++ h2o-r/tests/testdir_algos/stackedensemble/runit_stackedensemble_metalearner.R
@@ -46,8 +46,8 @@ stackedensemble.metalearner.test <- function() {
                                 training_frame = train,
                                 base_models = list(my_gbm, my_rf))
   # Check that metalearner_algorithm default is GLM w/ non-negative
-  expect_equal(stack0@parameters$metalearner_algorithm, NULL)
-  expect_equal(stack0@allparameters$metalearner_algorithm, "AUTO")
+  expect_equal(stack0@parameters$metalearner_algorithm, "glm")
+  expect_equal(stack0@allparameters$metalearner_algorithm, "glm")
   # Check that the metalearner is GLM w/ non-negative
   meta0 <- h2o.getModel(stack0@model$metalearner$name)
   expect_equal(meta0@algorithm, "glm")

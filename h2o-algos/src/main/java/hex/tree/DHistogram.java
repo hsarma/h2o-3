48900e47e7cd6a11c4b3aabb7024781db6017508
michalkurka <michalk@h2o.ai>
2020-05-07 08:49:17 -0400

    Remove unused method

--- h2o-algos/src/main/java/hex/tree/DHistogram.java
+++ h2o-algos/src/main/java/hex/tree/DHistogram.java
@@ -164,19 +164,6 @@ public final class DHistogram extends Iced {
     double[/*nbins*/] splitPts;
   }
 
-
-  public static int[] activeColumns(DHistogram[] hist) {
-    int[] cols = new int[hist.length];
-    int len=0;
-    for( int i=0; i<hist.length; i++ ) {
-      if (hist[i]==null) continue;
-      assert hist[i]._min < hist[i]._maxEx && hist[i].nbins() > 1 : "broken histo range "+ hist[i];
-      cols[len++] = i;        // Gather active column
-    }
-//    cols = Arrays.copyOfRange(cols, len, hist.length);
-    return cols;
-  }
-
   public void setMin( double min ) {
     long imin = Double.doubleToRawLongBits(min);
     double old = _min2;
